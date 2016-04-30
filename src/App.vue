7bdb1b0e3c1b235ac705693bcfa5d0211f317a32
DanielZhu <enterzhu@gmail.com>
2016-04-30 10:24:12 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: '生活不只是眼前的苟且，还有诗和远方'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var Vue = require('vue');
+var App = require('./App.vue');
+
+new Vue({
+  el: 'body',
+  components: { App }
+})
